7b85b212085cec88ebcec41785086ecbd616f299
Daniel Ridgway <ridginald@gmail.com>
2018-05-25 15:57:43 +0100

    fare stuff

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,18 +1,24 @@
 class Journey
-  # PENALTY_FARE = 6
-
+  PENALTY_FARE = 6
+  MINIMUM_FARE = 1
+  attr_reader :en_route
   attr_accessor :exit_station
 
   def initialize(entry_station = nil)
     @entry_station = entry_station
     @exit_station = nil
+    @en_route = true
   end
 
   def finish(exit_station)
     @exit_station = exit_station
+    @en_route = false
   end
 
   def fare
+    return PENALTY_FARE if @entry_station == nil
+    return PENALTY_FARE if @exit_station == nil
+    return MINIMUM_FARE
   end
 
   def complete?
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -12,6 +12,12 @@ describe Journey do
     it 'entry_station is optional' do
       expect{ journey }.not_to raise_error
     end
+    describe '#fare' do
+      it 'returns the penalty fare' do
+        journey.finish("Brook Green")
+        expect(journey.fare).to eq Journey::PENALTY_FARE
+      end
+    end
   end
 
   context 'initializes with an entry_station' do
@@ -27,6 +33,17 @@ describe Journey do
       it 'sets the exit_station' do
         journey.finish("Brook Green")
         expect(journey.exit_station).to eq "Brook Green"
+      end
+      it 'ends the journey' do
+        journey.finish("Brook Green")
+        expect(journey.en_route).to eq false
+      end
+    end
+
+    describe '#fare' do
+      it 'returns the minimum fare if both stations' do
+        journey.finish("Brook Green")
+        expect(journey.fare).to eq Journey::MINIMUM_FARE
       end
     end
   end
